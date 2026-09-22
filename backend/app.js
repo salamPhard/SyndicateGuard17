21b6286c667874369ff94ae5ab4900a6dbfaa26e
rayhaantechcare2020 <rayhaantechcare2020.com>
2026-09-22 17:15:37 +0100

    updated the port number

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ dotenv.config();  //Load env variables
 app.use(express.json());  //middleware to parse json
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 
