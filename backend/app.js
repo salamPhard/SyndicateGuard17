7463b209f7db362bf8fb71db3c9fd94ae92f6133
rayhaantechcare2020 <rayhaantechcare2020.com>
2026-09-22 15:43:51 +0100

    created config folder and file den update app.js

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+
+const dotenv = require('dotenv');
+
+dotenv.config();  //Load env variables
+
+app.use(express.json());  //middleware to parse json
+
+
+const PORT = process.env.PORT || 3000;
+
+
+
+
+const connectDB = require('./Config/dbConfig');
+connectDB();  //connect to MongoDB
+
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "uploads"))
+);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
