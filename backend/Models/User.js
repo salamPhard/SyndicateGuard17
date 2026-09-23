0243ffc63c8584b4f7795785f27d154db52ed771
Femicoder <emaholuwafemi@gmail.com>
2026-09-23 17:44:52 -0400

    Create user model for client registration

---
+++ backend/Models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  apiKey: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  isactive: {
+    type: Boolean,
+    default: true
+  },
+  
+  timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
